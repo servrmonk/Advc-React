31f0495d451b2abc3cca49ae98f8dd9832783e04
Raushan <roushankumarsingh2018@gmail.com>
2023-07-30 13:49:41 +0530

    Add all my files Input

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {
+    console.log("Title Changed!");//how to get the value
+    // console.log(event); check the value in target
+    //vanilla js : js without react
+    console.log(event.target.value);
+  };
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2024-12-23" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
